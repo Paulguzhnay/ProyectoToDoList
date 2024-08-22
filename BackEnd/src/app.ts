3eebf799129269c3cc9ead641e2166bc7589fc0e
yanditv <yankur44@gmail.com>
2024-08-21 21:04:13 -0500

    updated

--- BackEnd/src/app.ts
+++ BackEnd/src/app.ts
@@ -2,29 +2,31 @@ import { envs } from "./config";
 import { MongoDatabase } from "./data/mongodb";
 import { AppRoutes } from "./presentation/routes";
 import { Server } from "./presentation/server";
-import cors from 'cors';
+import cors from "cors";
 
 (() => {
-    main();
+  main();
 })();
 
 async function main() {
-    await MongoDatabase.connect({
-        mongoUrl: envs.MONGO_URL,
-        dbName: envs.MONGO_DB_NAME
-    });
+  await MongoDatabase.connect({
+    mongoUrl: envs.MONGO_URL,
+    dbName: envs.MONGO_DB_NAME,
+  });
 
-    const server = new Server({
-        port: envs.PORT,
-        routes: AppRoutes.routes
-    });
+  const server = new Server({
+    port: envs.PORT,
+    routes: AppRoutes.routes,
+  });
 
-    // Configuración de CORS
-    server.app.use(cors({
-        origin: 'http://localhost:4200', // Ajusta según sea necesario
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type', 'Authorization']
-    }));
+  // Configuración de CORS
+  server.app.use(
+    cors({
+      origin: ["*"], // Ajusta según sea necesario
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+    })
+  );
 
-    server.start();
+  server.start();
 }
